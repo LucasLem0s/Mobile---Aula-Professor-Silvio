ec366ef91d3044c9ca9b9fafa2a5170e02437030
LucasLem0s <lucaslemoss@live.com>
2025-08-18 21:56:45 -0300

    mobile

---
+++ App.js
@@ -0,0 +1,88 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View , FlatList, Image} from 'react-native';
+
+
+const DATA = [
+  {
+    id: '1',
+    nome: 'Harry Potter',
+    imagem: require('./assets/harry.png'),
+  },
+  {
+    id: '2',
+    nome: 'Hermione Granger',
+    imagem: require('./assets/hermione.png'),
+  },
+  {
+    id: '3',
+    nome: 'Ron Weasley',
+    imagem: require('./assets/ron.png'),
+  },
+  {
+    id: '4',
+    nome: 'Alvo Dumbledore',
+    imagem: require('./assets/alvo.png'),
+  },  
+  {
+    id:'5',
+    nome: 'Hagrid',
+    imagem: require('./assets/hagrid.png'), 
+
+  }
+];
+
+export default function App() {
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Image source={item.imagem} style={styles.imagem} />
+      <Text style={styles.texto}>{item.nome}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Personagens de Harry Potter</Text>
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+    backgroundColor: '#523909',
+  },
+  titulo: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  item: {
+    backgroundColor: '#d09116',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center',
+    borderRadius: 10, 
+  },
+  texto: {
+    fontSize: 20,
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  imagem: {
+    width: 120,
+    height: 180,
+    resizeMode: 'cover',
+    borderRadius: 8,
+  },
+});
+
